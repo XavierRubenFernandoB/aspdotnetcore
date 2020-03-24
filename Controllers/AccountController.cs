ad49b3dc72e426532546181fb4420cb7092c5585
Xavier Fernando <xavier.r.fernando@hotmail.com>
2020-03-24 16:13:17 +0530

    Identity user creation

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreProj.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NetCoreProj.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult RegisterView()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        // GET: /<controller>/
+        public async Task<IActionResult> RegisterView(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email
+                };
+
+                //Create the user
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                //login
+                if (result.Succeeded) 
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("listview","home");
+                }
+
+                //if error display on screen
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View();
+        }
+    }
+}
